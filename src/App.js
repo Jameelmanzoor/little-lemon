6fe05f4d94ece25cb2037751d08b1964673ece84
Jameel Ahmad <ahmadjameel963@gmail.com>
2023-07-09 10:07:48 +0500

    Added Basic naviagation in Nav & Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './Components/Footer';
 import Header from './Components/Header';
+import Nav from './Components/Nav';
 
 function App() {
   return (
     <>
      <Header />
+     <Nav />
+     <Footer />
     </>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+
+function Footer() {
+  return (
+    <footer>
+      <h2>This is Footer</h2>
+      <Nav />
+    </footer>
+  )
+}
+
+export default Footer
